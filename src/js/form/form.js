b0893aeea6a1993db09bedd648a7bc82892f9e4b
proflinervk <proflinervk@gmail.com>
2022-10-16 16:55:56 +0300

    Added js functionality in form

---
+++ src/js/form/form.js
@@ -0,0 +1,40 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+
+document.addEventListener( 'DOMContentLoaded', () => {
+	'use strict'
+
+	formOnClick()
+} )
+
+const formOnClick = () => {
+	const formWrapper = document.querySelector( '.form-wrapper' )
+	const formButton  = document.querySelectorAll( '.hero-button' )
+	const closeButton = document.querySelector( '.close-button' )
+	const targetElement  = document.querySelector( '#body-lock' )
+
+	if ( ! formWrapper && ! formButton ) return
+
+	formButton.forEach( button => {
+		button.addEventListener( 'click', () => {
+			if ( ! formWrapper.classList.contains( 'openned' ) )
+				formWrapper.classList.add( 'opened' )
+			else formWrapper.classList.remove( 'opened' )
+		} )
+	} )
+
+	closeButton.addEventListener( 'click', () => {
+		formWrapper.classList.remove( 'opened' )
+	} )
+
+	formWrapper.addEventListener( 'click', e => {
+        e.stopPropagation()
+
+        const target = e.target
+
+        if ( target.className && target.classList.contains( 'form-wrapper' ) )
+            formWrapper.classList.remove( 'opened' )
+    } )
+
+	if ( formWrapper.classList.contains( 'opened') ) disableBodyScroll( targetElement )
+	else enableBodyScroll( targetElement )
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
 import './common/common'
+
+//form
+import './form/form'
